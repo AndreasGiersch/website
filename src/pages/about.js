4abba7f88426327d0d8747f23f55e738d2b5c654
Andreas Giersch <agiersch@smail.uni-koeln.de>
2020-03-20 18:36:40 +0100

    added pages, changed initial design

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      This website is going to feature information about some individual
+      projects
+      <p>
+        <b>Add links to github/gitlab</b>
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/doggos.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Doggos = () => {
+  return (
+    <div>
+      This page is going to feature a list of doggo pictures, that are used for
+      my memory
+    </div>
+  )
+}
+
+export default Doggos
