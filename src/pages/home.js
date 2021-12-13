79684c3f7e1c9d16cc6ad342b74870bafac0aee1
AndreasGiersch <andreasgiersch@web.de>
2021-12-13 14:56:23 +0100

    updated to fit new server address

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,7 @@ const HomePage = () => {
         height="100"
         style={{ border: "0px" }}
         referrerPolicy="origin"
-        src="http://localhost:3000/widget?provider=eea%26lang=en%26longitude=10.67167%26latitude=53.85819%26token=3bd6fcb3-c0c4-4e5c-80a2-1796da12894a"
+        src="https://new-aq-widget.azurewebsites.net/widget?provider=eea%26lang=en%26longitude=10.67167%26latitude=53.85819%26token=3bd6fcb3-c0c4-4e5c-80a2-1796da12894a"
       ></iframe>
       <iframe
         id="widget_4"
@@ -20,7 +20,7 @@ const HomePage = () => {
         height="100"
         style={{ border: "0px" }}
         referrerPolicy="origin"
-        src="http://localhost:3000/widget?provider=eea&amp;lang=en&amp;longitude=10.67167&amp;latitude=53.85819&amp;token=3bd6fcb3-c0c4-4e5c-80a2-1796da12894a"
+        src="https://new-aq-widget.azurewebsites.net/widget?provider=eea&amp;lang=en&amp;longitude=10.67167&amp;latitude=53.85819&amp;token=3bd6fcb3-c0c4-4e5c-80a2-1796da12894a"
       ></iframe>
       <Link to="/">Go back to the index page</Link>
     </div>
