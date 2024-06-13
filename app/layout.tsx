d46fa0cad1f8514ed92d87443aa33f95e424cfc1
agiersch <>
2024-06-13 16:26:57 +0200

    updated app layout and page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import NavigationBar from '@/components/navbar/NavigationBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <NavigationBar />
+                <div className="flex min-h-screen flex-col items-center">{children}</div>
+            </body>
         </html>
     );
 }
