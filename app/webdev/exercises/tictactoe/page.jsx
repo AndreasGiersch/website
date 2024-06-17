06df93b866c24adf3456c41824c8f987385ace22
agiersch <>
2024-06-17 16:27:07 +0200

    continued implementing tic tac toe

--- app/webdev/exercises/tictactoe/page.jsx
+++ app/webdev/exercises/tictactoe/page.jsx
@@ -1,8 +1,9 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import './TicTacToe.css';
 import { useState } from 'react';
+import { Button } from '@nextui-org/button';
 
 function Square({ value, onSquareClick }) {
     return (
@@ -12,14 +13,49 @@ function Square({ value, onSquareClick }) {
     );
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [canReset, setCanReset] = useState(false);
+    const [winner, setWinner] = useState('');
 
-    const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    useEffect(() => {
+        const calcWinner = calculateWinner(squares);
+        if (calcWinner) {
+            setCanReset(true);
+            setWinner(calcWinner);
+        }
+    }, [squares]);
+
+    function resetGame() {
+        setSquares(Array(9).fill(null));
+        setXIsNext(true);
+        setCanReset(false);
+        setWinner('');
+    }
 
     function handleClick(i) {
-        if (squares[i]) {
+        if (winner || squares[i]) {
             return;
         }
         const nextSquares = squares.slice();
@@ -32,23 +68,37 @@ function Board() {
         setXIsNext(!xIsNext);
     }
 
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            <div className="game-status flex gap-4 w-full items-center">
+                {status}
+                <Button className="bg-slate-400 rounded-sm" disabled={!canReset} onClick={resetGame}>
+                    Reset
+                </Button>
             </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            <div className="game-board items-center justify-center">
+                <div className="board-row">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                </div>
             </div>
         </>
     );
@@ -57,8 +107,8 @@ function Board() {
 class TicTacToe extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
+            <div className="game w-[340px]">
+                <div className="game-board w-full">
                     <Board />
                 </div>
                 <div className="game-info">
