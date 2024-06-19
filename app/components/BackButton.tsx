1c7ab209a39983fe7d5d0d1e0a2bf5127d2dd05a
agiersch <>
2024-06-19 18:05:07 +0200

    create generic Back Button

---
+++ app/components/BackButton.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { Button } from '@nextui-org/react';
+import type { ButtonVariantProps } from '@nextui-org/react';
+import { PropsWithChildren } from 'react';
+import { ArrowLeftIcon } from '@heroicons/react/20/solid';
+
+type BackButtonProps = {
+    path: string;
+    color?: ButtonVariantProps['color'];
+    variant?: ButtonVariantProps['variant'];
+    className?: string;
+};
+
+const BackButton = (props: PropsWithChildren<BackButtonProps>) => {
+    const { path, color = 'default', variant = 'flat', className, children } = props;
+    return (
+        <Button
+            as={Link}
+            href={path}
+            color={color}
+            variant={variant}
+            type="button"
+            className={className}
+            startContent={<ArrowLeftIcon className="size-5" />}
+        >
+            {children}
+        </Button>
+    );
+};
+export default BackButton;
