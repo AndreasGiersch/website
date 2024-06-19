529f908631310074ebe6aeb7906f036eab53f459
agiersch <>
2024-06-19 18:04:47 +0200

    edit footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,12 +3,15 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <div className="bg-neutral-100 h-10 flex w-full items-center border-b border-divider backdrop-blur-lg backdrop-saturate-150 justify-start">
-            <div className="bg-neutral-100 container mx-auto my-auto px-6">
-                © Andreas Giersch. All rights reserved. Icons by{' '}
-                <Link className="underline" href={'https://icons8.com/'}>
-                    Icons8
-                </Link>
+        <div className="bg-gray-500 h-10 flex w-full items-center border-b border-divider backdrop-blur-lg backdrop-saturate-150 justify-start">
+            <div className="container mx-auto my-auto px-6 text-default-foreground">
+                <span>
+                    © Andreas Giersch. All rights reserved. Icons by{' '}
+                    <Link className="underline" href={'https://icons8.com/'}>
+                        Icons8.
+                    </Link>
+                </span>
+                <span> Created with Next.js, Tailwind CSS and NextUI.</span>
             </div>
         </div>
     );
