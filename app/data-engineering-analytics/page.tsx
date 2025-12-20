c6cb55b21719f65e536d33ab9e017d888201baef
agiersch <>
2025-12-20 13:48:57 +0100

    fix broken build

--- app/data-engineering-analytics/page.tsx
+++ app/data-engineering-analytics/page.tsx
@@ -1,6 +1,6 @@
+'use client';
 import BackButton from '@/components/BackButton';
 import CustomCard from '@/components/CustomCard/CustomCard';
-import React from 'react';
 
 const DataEngineeringAnalyticsPages = [
     {
